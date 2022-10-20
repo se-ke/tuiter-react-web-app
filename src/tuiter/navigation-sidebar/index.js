d74d624c1bc300454ea5190e7acaed48f700ac14
Sean Kelly <skboogi@live.com>
2022-10-19 21:09:17 -0400

    A6: Fix linting issues

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -6,31 +6,31 @@ const NavigationSidebar = (
 ) => {
     return (
         <div className="list-group">
-            <a className="list-group-item">Tuiter</a>
-            <a className={`list-group-item ${active === 'home'?'active':''}`}>
+            <li className="list-group-item">Tuiter</li>
+            <li className={`list-group-item ${active === 'home'?'active':''}`}>
                 Home
-            </a>
-            <a className={`list-group-item ${active === 'explore'?'active':''}`}>
+            </li>
+            <li className={`list-group-item ${active === 'explore'?'active':''}`}>
                 Explore
-            </a>
-            <a className={`list-group-item ${active === 'notifications'?'active':''}`}>
+            </li>
+            <li className={`list-group-item ${active === 'notifications'?'active':''}`}>
                 Notifications
-            </a>
-            <a className={`list-group-item ${active === 'messages'?'active':''}`}>
+            </li>
+            <li className={`list-group-item ${active === 'messages'?'active':''}`}>
                 Messages
-            </a>
-            <a className={`list-group-item ${active === 'bookmarks'?'active':''}`}>
+            </li>
+            <li className={`list-group-item ${active === 'bookmarks'?'active':''}`}>
                 Bookmarks
-            </a>
-            <a className={`list-group-item ${active === 'lists'?'active':''}`}>
+            </li>
+            <li className={`list-group-item ${active === 'lists'?'active':''}`}>
                 Lists
-            </a>
-            <a className={`list-group-item ${active === 'profile'?'active':''}`}>
+            </li>
+            <li className={`list-group-item ${active === 'profile'?'active':''}`}>
                 Profile
-            </a>
-            <a className={`list-group-item ${active === 'more'?'active':''}`}>
+            </li>
+            <li className={`list-group-item ${active === 'more'?'active':''}`}>
                 More
-            </a>
+            </li>
         </div>
     );
 };
